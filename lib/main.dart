b7439a8526ba1ae5c1936ae4c37ff23b7c261b27
Daniel Arevalo <acuentas9508@hotmail.com>
2019-12-09 16:16:41 -0500

    ya se termino la sesion de diseños de flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:diseno_app/src/pages/basico_page.dart';
+import 'package:diseno_app/src/pages/botones_pages.dart';
 import 'package:diseno_app/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // código para cambiar el color de las cositas de la bateria
+    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+    //   statusBarColor: Colors.white
+    // ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
-        'scroll': (BuildContext context) => ScrollPage()
+        'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage()
 
       },
     );
